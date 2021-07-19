7c0ef9b7e66509b43868a34ef2b5101ffe37b818
cklwblove <lwbhtml@163.com>
2021-07-19 14:49:05 +0800

    feat(vue3): 增加vue3的代码规则

--- test/vue3/custom-event-name-casing/.eslintrc.js
+++ test/vue3/custom-event-name-casing/.eslintrc.js
@@ -1,13 +1,8 @@
 module.exports = {
   rules: {
     /**
-     * 自定义事件名必须用 kebab-case 或 camelCase 风格
+     * 自定义事件名必须用 kebab-case 风格
      */
-    'vue/custom-event-name-casing': ['error',
-      'kebab-case' | 'camelCase',
-      {
-        'ignores': []
-      }
-    ]
+    'vue/custom-event-name-casing': 'error'
   }
 };
