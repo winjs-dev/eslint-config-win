ba7a0be40e3954f26985b55857a226e440ec114a
cklwblove <lwbhtml@163.com>
2021-07-19 16:38:03 +0800

    refactor(vue/experimental-script-setup-vars): 移除 vue/experimental-script-setup-vars

--- test/vue3/experimental-script-setup-vars/.eslintrc.js
+++ test/vue3/experimental-script-setup-vars/.eslintrc.js
@@ -3,6 +3,6 @@ module.exports = {
     /**
      * 避免 <script setup> 中的变量被视为未定义
      */
-    'vue/experimental-script-setup-vars': 'error',
+    'vue/experimental-script-setup-vars': 'off',
   },
 };
