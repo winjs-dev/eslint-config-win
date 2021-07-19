3b545b0d7a81ccfa09b97fa906a1fe568751ecee
cklwblove <lwbhtml@163.com>
2021-07-19 14:40:54 +0800

    refactor(vue/custom-event-name-casing): 自定义事件名必须用 kebab-case 或 camelCase 风格

--- test/vue3/custom-event-name-casing/.eslintrc.js
+++ test/vue3/custom-event-name-casing/.eslintrc.js
@@ -1,8 +1,13 @@
 module.exports = {
   rules: {
     /**
-     * 自定义事件名必须用 kebab-case 风格
+     * 自定义事件名必须用 kebab-case 或 camelCase 风格
      */
-    'vue/custom-event-name-casing': 'error',
-  },
+    'vue/custom-event-name-casing': ['error',
+      'kebab-case' | 'camelCase',
+      {
+        'ignores': []
+      }
+    ]
+  }
 };
